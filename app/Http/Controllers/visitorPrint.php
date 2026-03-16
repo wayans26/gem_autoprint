f6ce803fcfc5bc8d10cb0edb4767cda9b9cf4c06
wayans26 <wayan.setiawan98@gmail.com>
2026-03-16 22:12:22 +0800

    add model and registration

--- app/Http/Controllers/visitorPrint.php
+++ app/Http/Controllers/visitorPrint.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Utils\makeid;
 use App\Http\Utils\responseMessage;
+use App\Models\activity_history;
 use App\Models\registration;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -28,17 +30,36 @@ class visitorPrint extends Controller
             return responseMessage::responseMessage(0, "Visitor Not Found", 200);
         }
 
+        $checkinLocation = "AP";
+        $checkinTime = Carbon::now();
+        $checkinBy = $req->users->full_name;
+        $registerId = $visitor->id;
+        $firstRegister = false;
+
+        if (!activity_history::whereDate('CheckedInTime', $checkinTime->format('Y-m-d'))->exists()) {
+            activity_history::create([
+                'CheckedInTime' => $checkinTime,
+                'CheckedInLocation' => $checkinLocation,
+                'CheckedBy' => $checkinBy,
+                'registration_id' => $registerId,
+            ]);
+            $firstRegister = true;
+        }
+
+        $name = $visitor->Name === null ? $visitor->FirstName : $visitor->Name;
+        $job = $visitor->JobTitle === null ? $visitor->JobLevel : $visitor->JobTitle;
+
         $data_print = implode("\r\n", [
             "N",
             "q832",
             "Q609,24",
-            'A' . makeid::calculateCentreX($visitor->Name) . ',470,2,3,2,2,N,"' . makeid::esc($visitor->Name) . '"',
-            'A' . makeid::calculateCentreX($visitor->JobTitle) . ',420,2,3,2,2,N,"' . makeid::esc($visitor->JobTitle) . '"',
+            'A' . makeid::calculateCentreX($name) . ',470,2,3,2,2,N,"' . makeid::esc($name) . '"',
+            'A' . makeid::calculateCentreX($job) . ',420,2,3,2,2,N,"' . makeid::esc($job) . '"',
             'A' . makeid::calculateCentreX($visitor->Company) . ',370,2,3,2,2,N,"' . makeid::esc($visitor->Company) . '"',
             'b340,135,Q,m2,s6,eM,iA,"' . makeid::esc($visitor->Barcode) . '"',
             "P1"
         ]) . "\r\n";
-        $isPrinted = $visitor->IsPrinted;
+        $isPrinted = $visitor->IsPrinted === 0 || $firstRegister ? 0 : 1;
         $visitor->update([
             'IsPrinted' => 1,
             'LastCheckinLocation'   => "AP"
--- resources/js/Page/visitor_registration.vue
+++ resources/js/Page/visitor_registration.vue
@@ -1,3 +1,247 @@
 <template>
-    <h5>Visitor Registration</h5>
+    <div class="card">
+        <div class="card-header">
+            <h5>Visitor Registration Manual</h5>
+        </div>
+        <div class="card-body">
+            <form>
+                <div>
+                    <input ref="barcode_field" name="barcode" type="text" class="form-control" placeholder="Barcode *"
+                        v-model="barcode">
+                </div>
+                </input>
+                <br>
+                <p class="text-mute">{{ status }} | {{ printer_name }}</p>
+                <button type="button" class="btn btn-primary ml-1" @click="launchQzTray" v-show="!connected">Launch
+                    Printer</button>
+                <button type="button" class="btn btn-primary ml-1" @click="connectQzTray" v-show="!connected">Connect
+                    Printer</button>
+                <button type="button" class="btn btn-primary ml-1" @click="connectQzTray"
+                    v-show="connected">Register</button>
+            </form>
+        </div>
+    </div>
 </template>
+
+<script>
+import axios from 'axios';
+import swalNotif from '../Utils/swalNotif.js';
+import qz from "qz-tray";
+import notification from '../Utils/notification.js';
+
+export default {
+    data() {
+        return {
+            disabled: false,
+            loading: true,
+            barcode: "",
+            name: "",
+            title: "",
+            company: "",
+            email: "",
+            phone: "",
+            country: "",
+            exhibitions: "",
+            sub_exhibitions: "",
+            list_exhibitions: [],
+            list_sub_exhibitions: [],
+            list_country: [],
+
+            // Printer
+            status: "Printer Not Connected",
+            printer_name: "",
+            connected: false,
+            connecting: false,
+            showLaunchHint: false,
+            cfg: null,
+            data_print: "",
+            data_config: {
+                colorType: "color",
+                copies: 1,
+                density: 0,
+                duplex: false,
+                fallbackDensity: null,
+                interpolation: "bicubic",
+                jobName: null,
+                margins: 0,
+                orientation: null,
+                paperThickness: null,
+                printerTray: null,
+                rasterize: true,
+                rotation: 0,
+                scaleContent: true,
+                size: null,
+                units: "in",
+                altPrinting: false,
+                encoding: null,
+                endOfDoc: null,
+                perSpool: 1,
+            },
+        }
+    },
+    methods: {
+        esc(text = "") {
+            return String(text).replace(/"/g, "'");
+        },
+        async print_barcode() {
+            if (!this.connected) {
+                await this.connectQzTray();
+            }
+            const vm = this;
+            vm.globalLoader.show = true;
+            vm.disabled = true;
+            axios.post("/api/v1/web/visitor/print", {
+                'barcode': vm.barcode,
+            }, {
+                headers: {
+                    token: localStorage.getItem('token'),
+                }
+            }).then(async res => {
+                if (res.data.status == 1) {
+                    await vm.print();
+                }
+                else {
+                    notification.notif_error(res.data.message);
+                    vm.globalLoader.show = false;
+                    vm.disabled = false;
+                    vm.barcode = "";
+                }
+            }).catch(err => {
+                notification.notif_error("Error Internal Server");
+                vm.globalLoader.show = false;
+                vm.disabled = false;
+                vm.barcode = "";
+            }).finally(function () {
+
+            });
+        },
+        setupQzSecureOnce() {
+            qz.security.setCertificatePromise(function (resolve, reject) {
+                axios.get("/api/v1/web/qz/cert", {
+                    headers: { token: localStorage.getItem("token") },
+                })
+                    .then((res) => {
+                        const certPem = res.data.data;
+                        if (typeof certPem !== "string" || !certPem.includes("BEGIN CERTIFICATE")) {
+                            throw new Error("CERT is not valid PEM string");
+                        }
+                        resolve(certPem);
+                    });
+            });
+            qz.security.setSignatureAlgorithm("SHA256");
+            qz.security.setSignaturePromise(function (toSign) {
+                return function (resolve, reject) {
+                    // resolve();
+                    axios.post("/api/v1/web/qz/sign", {
+                        toSign: toSign
+                    }, {
+                        headers: { token: localStorage.getItem("token") },
+                    })
+                        .then((res) => {
+                            const certPem = res.data.data;
+                            resolve(certPem);
+                        });
+                };
+            });
+        },
+        launchQzTray() {
+            if (this.connected) {
+                swalNotif.info("Printer Already Connected");
+                return;
+            }
+            if (!this.connecting) {
+                window.location.href = "qz:launch";
+                setTimeout(async () => {
+                    await this.connectQzTray();
+                }, 5000);
+            }
+            else {
+                swalNotif.info("Connecting In Progress");
+                return;
+            }
+        },
+        async connectQzTray() {
+            if (this.connecting) {
+                swalNotif.info("Connecting In Progress");
+                return;
+            }
+            this.connecting = true;
+            if (qz.websocket.isActive()) {
+                this.connected = true;
+                this.connecting = false;
+                this.status = "Printer Already Connected";
+            }
+            else {
+                this.status = "Connectiong...";
+                await qz.websocket.connect({ retries: 5, delay: 1 }).then(async () => {
+                    this.connected = true;
+                    this.status = "Printer Connected";
+                    this.printer_name = await qz.printers.getDefault();
+                    // this.printer_name = "Argox CP-2140 PPLB"
+                    this.cfg = qz.configs.create(this.printer_name);
+                    this.connecting = false;
+                }).catch((err) => {
+                    swalNotif.error("Please Launch Printer First");
+                    this.status = "Printer Not Connected";
+                    this.connecting = false;
+                });
+            }
+        },
+        async safeDiconnect() {
+            try {
+                if (qz.websocket.isActive()) {
+                    this.status = "Disconnecting...";
+
+                    await qz.websocket.disconnect();
+                    this.status = "Printer Disconnected";
+                    this.connected = false;
+                    this.connecting = false;
+                    this.printer_name = "";
+                } else {
+                    this.status = "Printer Not Connected";
+                }
+            } catch (err) {
+                console.error("Failed to disconnect:", err);
+            }
+        },
+        async print() {
+            if (!this.connected) {
+                notification.notif_info("Please Launch Printer First");
+                return;
+            }
+            this.status = `Printing on ${this.printer_name}`;
+            if (!this.data_print) {
+                notification.notif_info("Data Not Found");
+                return;
+            }
+            if (this.cfg == null) {
+                notification.notif_info("Please Install Your Printer Driver");
+                return;
+            }
+            await qz.print(this.cfg, [{ type: "raw", format: "plain", data: this.data_print }]);
+            this.status = `Printed Successfully`;
+            this.globalLoader.show = false;
+            this.barcode = "";
+            this.disabled = false;
+        },
+        initValue() {
+
+        }
+
+    },
+    mounted() {
+        this.setupQzSecureOnce();
+        if (qz.websocket.isActive()) {
+            this.connected = true;
+            this.connecting = false;
+            this.status = "Printer Connected";
+        }
+        const vm = this;
+        this.loading = false;
+    },
+    beforeUnmount() {
+        this.safeDiconnect();
+
+    }
+}
+</script>
