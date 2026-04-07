41c8b15a811910a6bc7450e815676201248e3efd
wayans26 <wayan.setiawan98@gmail.com>
2026-04-07 18:04:10 +0800

    fix

--- app/Http/Controllers/registerController.php
+++ app/Http/Controllers/registerController.php
@@ -143,7 +143,7 @@ class registerController extends Controller
             'A' . makeid::calculateCentreX($req->name, $textSize) . ',' . ($startYText - ($pengurangan * 0)) . ',2,' . $textSize . ',' . $vhmul . ',' . $vhmul . ',N,"' . Str::upper(makeid::esc($req->name))  . '"',
             'A' . makeid::calculateCentreX($req->title, $textSize) . ',' . ($startYText - ($pengurangan * 1)) . ',2,' . $textSize . ',' . $vhmul . ',' . $vhmul . ',N,"' . Str::upper(makeid::esc($req->title))  . '"',
             ...$company,
-            'b360,135,Q,m2,' . $barcodeSize . ',eH,"' . makeid::esc($barcode) . '"',
+            'b350,150,Q,m2,' . $barcodeSize . ',eH,"' . makeid::esc($barcode) . '"',
             "P1"
         ]) . "\r\n";
 
