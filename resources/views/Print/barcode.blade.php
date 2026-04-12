1a32e8925a46bc064ce54e08c957a853454ac410
wayans26 <wayan.setiawan98@gmail.com>
2026-04-12 15:30:00 +0800

    fix css

--- resources/views/Print/barcode.blade.php
+++ resources/views/Print/barcode.blade.php
@@ -1,6 +1,92 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <title>QR Label</title>
+    <style>
+        @page {
+            margin: 0;
+        }
+
+        html,
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            font-family: DejaVu Sans, sans-serif;
+            color: #000000;
+            background: #ffffff;
+        }
+
+        .page {
+            position: relative;
+            width: 100%;
+            height: 100%;
+        }
+
+        .safe-area {
+            position: absolute;
+            top: 4mm;
+            right: 4mm;
+            bottom: 4mm;
+            left: 4mm;
+            display: table;
+            width: auto;
+            height: auto;
+            table-layout: fixed;
+        }
+
+        .content {
+            display: table-cell;
+            vertical-align: middle;
+            text-align: center;
+        }
+
+        .name {
+            font-size: 20pt;
+            font-weight: 700;
+            line-height: 1.15;
+            margin-bottom: 3mm;
+        }
+
+        .job {
+            font-size: 13pt;
+            font-weight: 700;
+            line-height: 1.2;
+            margin-bottom: 1.5mm;
+        }
+
+        .company {
+            font-size: 12pt;
+            line-height: 1.2;
+            margin-bottom: 4mm;
+        }
+
+        .qrcode-wrapper {
+            width: 32mm;
+            height: 32mm;
+            margin: 0 auto 2mm auto;
+            text-align: center;
+        }
+
+        .qrcode-image {
+            display: block;
+            width: 32mm;
+            height: 32mm;
+            margin: 0 auto;
+        }
+
+        .barcode-text {
+            font-size: 11pt;
+            font-weight: 700;
+            letter-spacing: 0.6px;
+            line-height: 1.2;
+        }
+    </style>
+</head>
+
 <body>
     <div class="page">
         <div class="safe-area">
