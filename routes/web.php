2f81204a16ada64c27ad942996637befb0131fdb
wayans26 <wayan.setiawan98@gmail.com>
2026-04-12 22:25:50 +0800

    disable path test

--- routes/web.php
+++ routes/web.php
@@ -24,32 +24,32 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use ZanySoft\Zip\Facades\Zip;
 
 
-Route::get('/test', function () {
-    // $barcodeSvg = (new TypeCode128())->getBarcode("YANAAAA");
-    // $renderer = new SvgRenderer();
-    // $renderer->setSvgType($renderer::TYPE_SVG_INLINE);
-    // $barcodeSvgRendered = $renderer->render($barcodeSvg, 360, 70);
-    // $qrcode = QrCode::format('svg')
-    //     ->size(220)
-    //     ->margin(1)
-    //     ->errorCorrection('H')
-    //     ->generate("AAAA");
-    $qrcode = QrCode::format('png')
-        ->size(220)
-        ->margin(1)
-        ->errorCorrection('H')
-        ->generate("YAN AHJAHG");
-    $widthMM = 104.1;
-    $heightMM = 76.2;
-    $urlQr = "data:image/png;base64," . base64_encode($qrcode);
-    // dd($urlQr);
-    return view('Print.barcode',  [
-        'nama'  => "TEST",
-        'job'   => "Job",
-        'company' => "COMPANY",
-        'barcodeSvg' => $urlQr,
-    ]);
-});
+// Route::get('/test', function () {
+//     // $barcodeSvg = (new TypeCode128())->getBarcode("YANAAAA");
+//     // $renderer = new SvgRenderer();
+//     // $renderer->setSvgType($renderer::TYPE_SVG_INLINE);
+//     // $barcodeSvgRendered = $renderer->render($barcodeSvg, 360, 70);
+//     // $qrcode = QrCode::format('svg')
+//     //     ->size(220)
+//     //     ->margin(1)
+//     //     ->errorCorrection('H')
+//     //     ->generate("AAAA");
+//     $qrcode = QrCode::format('png')
+//         ->size(220)
+//         ->margin(1)
+//         ->errorCorrection('H')
+//         ->generate("YAN AHJAHG");
+//     $widthMM = 104.1;
+//     $heightMM = 76.2;
+//     $urlQr = "data:image/png;base64," . base64_encode($qrcode);
+//     // dd($urlQr);
+//     return view('Print.barcode',  [
+//         'nama'  => "TEST",
+//         'job'   => "Job",
+//         'company' => "COMPANY",
+//         'barcodeSvg' => $urlQr,
+//     ]);
+// });
 
 Route::get('/', function (Request $req) {
     // dd(Hash::make("admin"));
